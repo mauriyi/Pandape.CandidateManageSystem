25e2d1d221daa0736d048dd4ac9d3111363ce64e
Mauricio Bedoya <mauriyi@hotmail.com>
2025-05-15 13:11:23 -0500

    feat(command): define UpdateCandidateCommand para actualizar información del candidato

---
+++ Application/Features/Candidates/Commands/UpdateCandidate/UpdateCandidateCommand.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.DTOs;
+using MediatR;
+
+namespace Application.Features.Candidates.Commands.UpdateCandidate
+{
+    /// <summary>
+    /// Comando para actualizar la información de un candidato existente.
+    /// </summary>
+    /// <param name="Candidate">Datos actualizados del candidato.</param>
+    /// <returns>Devuelve el <see cref="CandidateDto"/> actualizado</returns>
+    public record UpdateCandidateCommand(CandidateDto Candidate)
+        : IRequest<CandidateDto>;
+}

1794986b59ed924c2f9d31f59518d68127f65d37
Mauricio Bedoya <mauriyi@hotmail.com>
2025-05-15 13:24:42 -0500

    feat(web): se crean CandidateViewModel y CandidateExperienceViewModel para la capa de presentación

---
+++ Web/ViewModels/Candidate/CandidateExperienceViewModel.cs
@@ -0,0 +1,30 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web.ViewModels.Candidate
+{
+    public class CandidateExperienceViewModel
+    {
+
+        [StringLength(100)]
+        public required string Company { get; set; }
+
+        [StringLength(100)]
+        public required string Job { get; set; }
+
+        [StringLength(4000)]
+        public required string Description { get; set; }
+
+        [Column(TypeName = "decimal(8, 2)")]
+        public required decimal Salary { get; set; }
+
+        public required DateTime BeginDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public required DateTime InsertDate { get; set; }
+
+        public DateTime? ModifyDate { get; set; }
+    }
+}
---
+++ Web/ViewModels/Candidate/CandidateViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.ViewModels.Candidate
+{
+    public class CandidateViewModel
+    {
+
+        public int IdCandidate { get; set; }
+
+        [StringLength(50)]
+        public required string Name { get; set; }
+
+        [StringLength(150)]
+        public required string Surname { get; set; }
+
+        public required DateTime Birthdate { get; set; }
+
+        [StringLength(250)]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        public required DateTime InsertDate { get; set; }
+
+        public DateTime? ModifyDate { get; set; }
+
+        public List<CandidateExperienceViewModel> Experiences { get; set; } = new List<CandidateExperienceViewModel>();
+    }
+}
